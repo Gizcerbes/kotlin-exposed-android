09c6a25ee5c522b77085c9154e561d513405b3c3
Andrei Naumets <gizcerbes@gmail.com>
2024-09-17 13:26:20 +0300

    clean

--- app/src/main/java/com/uogames/kotlin_exposed_test/db/TestTable.kt
+++ app/src/main/java/com/uogames/kotlin_exposed_test/db/TestTable.kt
@@ -1,8 +1,5 @@
 package com.uogames.kotlin_exposed_test.db
 
-import android.content.Context
-import android.util.Log
-import org.jetbrains.exposed.dao.Entity
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.ResultRow
@@ -15,44 +12,44 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.UUID
 
 object TestTable : UUIDTable() {
-	val text = text("hello")
+    val text = text("hello")
 
-	data class Test(
-		val id: UUID = UUID.randomUUID(),
-		val text: String
-	)
+    data class Test(
+        val id: UUID = UUID.randomUUID(),
+        val text: String
+    )
 
 
-	fun toDTO(result: ResultRow): Test{
-		return Test(
-			id = result[id].value,
-			text = result[text]
-		)
-	}
+    fun toDTO(result: ResultRow): Test {
+        return Test(
+            id = result[id].value,
+            text = result[text]
+        )
+    }
 }
 
 fun initDB(dbPath: String) {
-	Database.connect("jdbc:sqlite:${dbPath}")
-	transaction {
-		SchemaUtils.create(TestTable)
-	}
+    Database.connect("jdbc:sqlite:${dbPath}")
+    transaction {
+        SchemaUtils.create(TestTable)
+    }
 
 }
 
 
 fun TestTable.save(test: TestTable.Test) = transaction {
-	TestTable.insert {
-		it[id] = test.id
-		it[text] = test.text
-	}
+    TestTable.insert {
+        it[id] = test.id
+        it[text] = test.text
+    }
 }
 
 fun TestTable.getAll() = transaction {
-	TestTable.selectAll().map { toDTO(it) }
+    TestTable.selectAll().map { toDTO(it) }
 }
 
 fun TestTable.delete(test: TestTable.Test) = transaction {
-	TestTable.deleteWhere {
-		TestTable.id eq test.id
-	}
+    TestTable.deleteWhere {
+        TestTable.id eq test.id
+    }
 }
